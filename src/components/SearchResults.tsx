d26afd7707e78587479c6a6f4e2d787b38e26028
jvolima <jvolima2004@gmail.com>
2022-03-23 18:18:57 -0300

    Aprendendo a performar apps em React

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,30 @@
+import { useMemo } from "react";
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+  results: Array<{
+    id: number;
+    price: number;
+    title: string;
+  }>
+}
+
+export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results])
+
+  return (
+    <div>
+      <h2>{totalPrice}</h2>
+
+      {results.map(result => {
+        return (
+          <ProductItem key={result.id} product={result}/>
+        )
+      })}
+    </div>
+  )
+}
